4da20dfba11fd31b4ff4680db2c06f8fbfc1038a
hasin01 <81334215+hasin01@users.noreply.github.com>
2023-05-05 10:30:42 +0000

    Deploying to gh-pages from @ hasin01/Test@0ab92fef7d1c4c5374adbf10553090b30d081373 🚀

---
+++ index.9523562f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".div");console.log(e),e.addEventListener("click",(e=>{}));
+//# sourceMappingURL=index.9523562f.js.map
---
+++ index.a1cd7b45.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".div");console.log(e),e.addEventListener("click",(function(e){}))}();
+//# sourceMappingURL=index.a1cd7b45.js.map
